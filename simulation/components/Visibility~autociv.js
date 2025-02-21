712d1ad287c77434df0c40dd75741c3c6423825d
sl5net <sl5softwarelab@gmail.com>
2025-02-21 22:29:07 +0100

    fixed typo

--- simulation/components/Visibility~autociv.js
+++ simulation/components/Visibility~autociv.js
@@ -1,16 +1,15 @@
 if (!Visibility.prototype.OnDestroy)
-    Visibility.prototype.OnDestroy = function () { };
+  Visibility.prototype.OnDestroy = function () { };
 
 let cmpGUIInterface = Engine.QueryInterface(SYSTEM_ENTITY, IID_GuiInterface);
-autociv_patchApplyN(Visibility.prototype, "OnDestroy", function (target, that, args)
-{
-    if (cmpGUIInterface && cmpGUIInterface.autociv && cmpGUIInterface.autociv.corpse && cmpGUIInterface.autociv.corpse.entities) {
-        console.log("Removing entity " + that.entity + " from corpse list.");
-        cmpGUIInterface.autociv.corpse.entities.delete(that.entity);
-    } else {
-        console.log("Warning: Unable to remove entity " + that.entity + " from corpse list.");
-    }
-    return target.apply(that, args);
+autociv_patchApplyN(Visibility.prototype, "OnDestroy", function (target, that, args) {
+  if (cmpGUIInterface && cmpGUIInterface.autociv && cmpGUIInterface.autociv.corpse && cmpGUIInterface.autociv.corpse.entities) {
+    console.log("Removing entity " + that.entity + " from corpse list.");
+    cmpGUIInterface.autociv.corpse.entities.delete(that.entity);
+  } else {
+    print("Warning: Unable to remove entity " + that.entity + " from corpse list.");
+  }
+  return target.apply(that, args);
 })
 
 Engine.ReRegisterComponentType(IID_Visibility, "Visibility", Visibility);
